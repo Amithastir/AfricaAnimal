7e302b4ea100d6611296c70d58115b8afc37ed24
Amit Sharma <amithastir@gmail.com>
2025-06-22 11:47:59 +0530

    update the tabbar ui.

--- Africa Animal/Modules/RootTabBar/MainTabView.swift
+++ Africa Animal/Modules/RootTabBar/MainTabView.swift
@@ -8,26 +8,44 @@
 import SwiftUI
 
 struct MainTabView: View {
+    @State private var selectedTab = 0
+
+    init() {
+        UITabBar.appearance().barTintColor = UIColor.black
+        UITabBar.appearance().backgroundColor = UIColor.black
+        UITabBar.appearance().isTranslucent = false
+    }
+
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab) {
             HomeModuleBuilder.build()
                 .tabItem {
-                    Image(systemName: "house.fill")
-                    Text("Home")
+                    Image(systemName: "square.grid.2x2.fill")
+                    Text("Browse")
                 }
+                .tag(0)
 
-            Text("Favorites")
+            Text("Watch") // need to replace with video module.
                 .tabItem {
-                    Image(systemName: "star.fill")
-                    Text("Favorites")
+                    Image(systemName: "play.rectangle.fill")
+                    Text("Watch")
                 }
+                .tag(1)
 
-            Text("Profile")
+            Text("Fav") // need to replace with favorites module.
                 .tabItem {
-                    Image(systemName: "person.fill")
-                    Text("Profile")
+                    Image(systemName: "heart.fill")
+                    Text("Fav")
                 }
+                .tag(2)
+
+            Text("Settings") // need to replace with setting module.
+                .tabItem {
+                    Image(systemName: "gearshape.fill")
+                    Text("Settings")
+                }
+                .tag(3)
         }
-        .accentColor(.orange)
+        .accentColor(.accent) // Selected item color
     }
 }
